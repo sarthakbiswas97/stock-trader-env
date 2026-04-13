130de0a6d965cc58c8233fac4efe187483f0a9da
sarthaksbiswas97 <sarthak.sbiswas@gmail.com>
2026-04-14 04:49:01 +0530

    feat: add MLflow experiment tracking and standardized evaluation harness

--- training/__init__.py
+++ training/__init__.py
@@ -2,5 +2,14 @@
 
 from training.gym_wrapper import StockTradingGymEnv
 from training.data_splits import DataSplit, SPLITS
+from training.experiment import ExperimentTracker
+from training.evaluate import evaluate_agent, EvalResults
 
-__all__ = ["StockTradingGymEnv", "DataSplit", "SPLITS"]
+__all__ = [
+    "StockTradingGymEnv",
+    "DataSplit",
+    "SPLITS",
+    "ExperimentTracker",
+    "evaluate_agent",
+    "EvalResults",
+]
